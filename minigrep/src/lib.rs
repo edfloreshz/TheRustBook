7bf6dab8a146f25f6cba1878bb2289171724b57d
Eduardo Flores <edfloreshz@gmail.com>
2020-06-07 00:44:03 -0700

    tests and search implementation

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,3 +22,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let _ = fs::read_to_string(config.filename)?;
   Ok(())
 }
+
+pub fn search<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
+  vec![]
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "productive";
+    assert_eq!(vec!["productive"], search(query, contents));
+  }
+}
